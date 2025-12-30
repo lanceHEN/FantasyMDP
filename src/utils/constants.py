60e04c1b398da4751b5def368edcd58be1eef321
lancehendricks <hendricks.la@northeastern.edu>
2025-12-29 20:13:13 -0800

    Added constants file

---
+++ src/utils/constants.py
@@ -0,0 +1,12 @@
+"""Utility constants for roster limitations."""
+
+NUM_POS = 6 # number of positions
+POS = ['QB', 'RB', 'WR', 'TE', 'DST', 'K']
+POS_LIMITS = { # players to draft per position
+    'QB': 1,
+    'RB': 3,
+    'WR': 3,
+    'TE': 1,
+    'DST': 1,
+    'K': 1
+}
